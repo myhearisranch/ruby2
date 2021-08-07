0fba15abc0830f4572538d76651ca6b86200ed91
EC2 Default User <ec2-user@ip-172-31-69-243.ec2.internal>
2021-08-07 22:36:41 +0000

    [Finsh]2-7

---
+++ break.rb
@@ -0,0 +1,9 @@
+i=1
+while i<=10 do
+  if i == 5
+    puts "処理を終了します" #処理を終了する条件
+    break
+end
+  puts i　#whileによって行う処理
+  i +=1
+end
---
+++ lesson7-2.rb
@@ -0,0 +1,53 @@
+puts "計算を始めます"
+puts "何回繰り返しますか"
+
+input=gets.to_i
+
+i=1
+while i<=input do
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+
+  a=gets.to_i
+  b=gets.to_i
+
+  puts a+b
+  puts a-b
+  puts a*b
+  puts a/b
+
+  i+=1
+end
+
+puts "計算を終了します"
+
+
+=begin
+puts "計算をはじめます"
+puts "何回繰り返しますか？"
+
+input = gets.to_i （繰り返す回数）
+
+i = 1　（iは何回目か表す）
+
+while i <= input do　（iとinput（繰り返したい回数）が同じになるまで処理を繰り返す）
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+
+  a = gets.to_i
+  b = gets.to_i
+
+  puts "a=#{a}"
+  puts "b=#{b}"
+
+  puts "計算結果を出力します"
+  puts "a+b=#{a + b}"
+  puts "a-b=#{a - b}"
+  puts "a*b=#{a * b}"
+  puts "a/b=#{a / b}"
+
+  i += 1
+end
+
+puts "計算を終了します"
+=end
